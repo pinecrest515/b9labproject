edc18b79a0241ea4183859f316ff254e8f31018a
pinecrest515 <codelearning515@gmail.com>
2016-08-21 17:53:08 +0700

    Update Project.sol

--- Project.sol
+++ Project.sol
@@ -1,5 +1,6 @@
 contract Project {
-    struct Project {
+    // constructor and struct storing info of owner, contribution goal, deadline, etc
+    struct ProjectData {
         address owner;
         uint contributed;
         uint goal;
@@ -11,49 +12,30 @@ contract Project {
         uint amount;
     };
 
-    // Start a new campaign.
-    function start(address recipient, uint256 goal, uint256 deadline) returns (uint id) {
-        campaigns[nextCampaignId] = CampaignData(recipient, 0, goal, deadline);
-        nextCampaignId ++;
-        id = nextCampaignId;
-    }
-
-    // Contribute to the compaign with id $(campaignId).
-    function contribute(uint256 campaignId) {
-        var campaign = campaigns[campaignId];
-        if (campaign.deadline == 0) // check for non-existing campaign
+    // Start a new campaign funding project and contribute to the funding
+    function fund(address recipient, uint256 goal, uint256 deadline) {
+        var project = ProjectData(recipient, 0, goal, deadline);
+        if (project.deadline == 0) // check for non-existing campaign funding project
             return;
-        campaign.contributed += transaction.value;
-        campaign.contributions[campaign.num_contributions] =
+        project.contributed += transaction.value;
+        project.contributions =
                       Contribution(transaction.sender, transaction.value);
-        campaign.num_contributions++;
-    }
 
     // Check whether the funding goal of the campaign with id $(campaignId)
     // has been reached and transfer the money.
-    function checkGoalReached(uint256 campaignId) returns (bool reached)
-    {
-        var campaign = campaigns[campaignId];
-        if (campaign.deadline > 0 && campaign.contributed >= campaign.goal) {
-            send(campaign.recipient, campaign.contributed);
-            // clear storage, we have to do it explicitly for the mapping since
-            // it is not possible to enumerate all set keys.
-            for (uint i = 0; i < campaign.num_contributions; i++) {
-                delete campaign.contributions[i]; // zero out its members
-            }
-            delete campaign;
-            reached = true;
+        if (project.deadline > 0 && project.contributed >= project.goal) {
+            payout(project.recipient, project.contributed);
+            // clear storage, we have to do it explicitly 
+            delete project.contributions[i]; // zero out its members
+            delete project;
         }
     }
 
     // Check whether the deadline of the campaign with id $(campaignId) has
     // passed. In that case, return the contributed money and delete the
     // campaign.
-    function checkExpired(uint campaignId) returns (bool expired)
-    {
         expired = false;
-        var campaign = campaigns[campaignId];
-        if (campaign.deadline > 0 && block.timestamp > campaign.deadline) {
+        if (project.deadline > 0 && block.timestamp > project.deadline) {
             // pay out the contributors
             for (uint i = 0; i < campaign.num_contributors; i++) {
                 send(campaign.contributions[i].contributor,
@@ -64,14 +46,14 @@ contract Project {
             expired = true;
         }
     }
-
+    }
+    
     // Refund() This function sends all individual contributions back to the respective contributor
     function refund(uint campaignId) returns (uint amount)
     {
-        amount = campaigns[campaignId].contributed;
+        amount = project.contributed;
     }
 
 state:
-    uint nextCampaignId;
-    mapping(uint256 => CampaignData) campaigns;
+    mapping(uint256 => ProjectData) project;
 };
