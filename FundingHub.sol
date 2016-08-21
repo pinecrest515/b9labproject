ddbeda2f6f1a81b7a48d152153c115e9f1a8f2f8
pinecrest515 <codelearning515@gmail.com>
2016-08-21 20:05:35 +0700

    Update FundingHub.sol

--- FundingHub.sol
+++ FundingHub.sol
@@ -1,9 +1,19 @@
 contract FundingHub {
 
   function createProject {
+  address public recipient;
+  unit256 public goal;
+  unit256 public deadline;
+  
+  var project = ProjectData(recipient, 0, goal, deadline)
+  
   }
   
   function contribute {
+  unit256 contributedvalue;
+  
+  // call fund() function 
+  fund(contributedvalue);
   }
   
 }
